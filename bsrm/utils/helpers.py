5a7501e5650d05fc2ea3e2dd9d47e6e26ad2b8f4
Paudyal <Pragya.Paudyal@ons.gov.uk>
2026-07-08 10:37:27 +0100

    fixed ruff error for docstring  and qa_df column order

--- bsrm/utils/helpers.py
+++ bsrm/utils/helpers.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 
 def create_test_dataframe(data: list[tuple], **kwargs) -> pd.DataFrame:  # noqa ANN003
-    """Create a Pandas dataframe from a human-readable tuple
-    with a header for unit tests.
+    """Create a Pandas dataframe from a human-readable tuple.
+
+    With a header for unit tests.
     """
     df = pd.DataFrame.from_records(data[1:], columns=data[0], **kwargs)
     return df
